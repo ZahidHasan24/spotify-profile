3b71086ab1e9c2120bc1ab5fdadb6093be169061
Zahid Hasan <hzahid2999@gmail.com>
2022-10-01 14:20:40 +0600

    add axios to fetch data, add error utils to get errors and show some profile data after login with fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import { useEffect, useState } from "react";
 import { accessToken, logout } from "./utils/spotify";
+import { getCurrentUserProfile } from "./service";
+import { catchErrors } from "./utils/error";
 import "./App.css";
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+    catchErrors(fetchData());
   }, []);
+
+  const fetchData = async () => {
+    const res = await getCurrentUserProfile();
+    setProfile(res.data);
+  };
 
   return (
     <div className="App">
@@ -20,6 +29,15 @@ function App() {
           <>
             <h1>Logged in!</h1>
             <button onClick={logout}>Log Out </button>
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Avatar" />
+                )}
+              </div>
+            )}
           </>
         )}
       </header>
