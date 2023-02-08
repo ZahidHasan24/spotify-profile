77caa505d0652eb73afe364e12ee768d05d8c97a
Zahid Hasan <hzahid2999@gmail.com>
2023-02-08 22:00:12 +0600

    refactor server code

--- routes/callback.js
+++ routes/callback.js
@@ -9,43 +9,6 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const FRONTEND_URI = process.env.FRONTEND_URI;
 
-const getUserProfile = async (data) => {
-  const { access_token, token_type } = data;
-  try {
-    const response = await axios.get("https://api.spotify.com/v1/me", {
-      headers: {
-        Authorization: `${token_type} ${access_token}`,
-      },
-    });
-    return {
-      status: "ok",
-      data: response.data,
-    };
-  } catch (error) {
-    return {
-      status: "error",
-      data: response,
-    };
-  }
-};
-
-const getRefreshToken = async (refresh_token) => {
-  try {
-    const response = await axios.get(
-      `http://localhost:8000/refresh_token?refresh_token=${refresh_token}`
-    );
-    return {
-      status: "ok",
-      data: response.data,
-    };
-  } catch (error) {
-    return {
-      status: "error",
-      data: response,
-    };
-  }
-};
-
 router.get("/", async (req, res) => {
   const code = req.query.code || null;
   const data = querystring.stringify({
