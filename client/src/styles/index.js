a56b6863df53438cb61ce87bc98a74c6b74d45fc
Zahid Hasan <hzahid2999@gmail.com>
2023-03-18 21:43:28 +0600

    added StyledSection styles and SectionWrapper component

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -2,5 +2,6 @@ import GlobalStyle from "./GlobalStyle";
 import StyledLogoutButton from "./StyledLogoutButton";
 import StyledHeader from "./StyledHeader";
 import StyledGrid from "./StyledGrid";
+import StyledSection from "./StyledSection";
 
 export { GlobalStyle, StyledLogoutButton, StyledHeader, StyledGrid };
