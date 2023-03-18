e4ab5114415160eaf52743e453fb68457749b9ae
Zahid Hasan <hzahid2999@gmail.com>
2023-03-18 20:48:43 +0600

    added StyledHeader in profile page with showing number of followers and playlists using api

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,29 +1,46 @@
 import { useState, useEffect } from "react";
 import { catchErrors } from "../utils/error";
-import { getCurrentUserProfile } from "../service";
+import { getCurrentUserProfile, getCurrentUserPlaylists } from "../service";
+import { StyledHeader } from "../styles";
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
+  const [playlists, setPlaylists] = useState(null);
 
   useEffect(() => {
     catchErrors(fetchData());
   }, []);
 
   const fetchData = async () => {
-    const res = await getCurrentUserProfile();
-    setProfile(res.data);
+    const userProfileRes = await getCurrentUserProfile();
+    const userPlaylistsRes = await getCurrentUserPlaylists();
+    setProfile(userProfileRes.data);
+    setPlaylists(userPlaylistsRes.data);
   };
 
   return (
     <>
       {profile && (
-        <div>
-          <h1>{profile.display_name}</h1>
-          <p>{profile.followers.total} followers</p>
-          {profile.images.length && profile.images[0].url && (
-            <img src={profile.images[0].url} alt="Avatar" />
-          )}
-        </div>
+        <StyledHeader type="user">
+          <div className="header__inner">
+            {profile.images.length && profile.images[0].url && (
+              <img
+                src={profile.images[0].url}
+                alt="Avatar"
+                className="header__img"
+              />
+            )}
+            <div>
+              <div className="header__overline">Profile</div>
+
+              <h1 className="header__name">{profile.display_name}</h1>
+              <p className="header__meta">
+                {playlists && <span>{playlists.total} Playlists</span>}
+                <span>{profile.followers.total} Followers</span>
+              </p>
+            </div>
+          </div>
+        </StyledHeader>
       )}
     </>
   );
--- client/src/service/index.js
+++ client/src/service/index.js
@@ -9,10 +9,17 @@ axios.defaults.baseURL = "https://api.spotify.com/v1";
 axios.defaults.headers["Authorization"] = `Bearer ${accessToken}`;
 axios.defaults.headers["Content-Type"] = "application/json";
 
-
 /**
  * Get Current User's Profile
  * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-current-users-profile
  * @returns {Promise}
  */
- export const getCurrentUserProfile = () => axios.get('/me');
+export const getCurrentUserProfile = () => axios.get("/me");
+
+/**
+ * Get a List of Current User's Playlists
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-a-list-of-current-users-playlists
+ * @returns {Promise}
+ */
+export const getCurrentUserPlaylists = (limit = 20) =>
+  axios.get(`/me/playlists?limit=${limit}`);
