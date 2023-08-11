3fd96ba9d341d80ce0fe28bf4482635295bc7b8e
Zahid Hasan <hzahid2999@gmail.com>
2023-08-11 22:18:36 +0600

    added functionlities in playlists to show all playlists if any

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -3,25 +3,48 @@ import SectionWrapper from "../components/SectionWrapper";
 import { catchErrors } from "../utils/error";
 import { getCurrentUserPlaylists } from "../service";
 import PlaylistsGrid from "../components/PlayListsGrid";
+import axios from "axios";
 
 const Playlists = () => {
+  const [playlistsData, setPlaylistsData] = useState(null);
   const [playlists, setPlaylists] = useState(null);
+
+  const fetchData = async () => {
+    const userPlaylistsRes = await getCurrentUserPlaylists();
+    setPlaylistsData(userPlaylistsRes.data);
+  };
+
+  const fetchMoreData = async (nextUrl) => {
+    if (nextUrl) {
+      const { data } = await axios.get(nextUrl);
+      setPlaylistsData((prevData) => ({
+        ...prevData,
+        items: [...prevData.items, ...data.items],
+        next: data.next,
+      }));
+    }
+  };
 
   useEffect(() => {
     catchErrors(fetchData());
   }, []);
 
-  const fetchData = async () => {
-    const userPlaylistsRes = await getCurrentUserPlaylists();
-    setPlaylists(userPlaylistsRes.data);
-  };
+  useEffect(() => {
+    if (!playlistsData) {
+      return;
+    }
+
+    // Update playlists when playlistsData changes
+    setPlaylists(playlistsData.items);
+
+    // Fetch more data when playlistsData.next is available
+    catchErrors(fetchMoreData(playlistsData.next));
+  }, [playlistsData]);
 
   return (
     <main>
-      <SectionWrapper title="Playlists" breadcrumb={true}>
-        {playlists && playlists.items && (
-          <PlaylistsGrid playlists={playlists?.items} />
-        )}
+      <SectionWrapper title="Public Playlists" breadcrumb={true}>
+        {playlists && <PlaylistsGrid playlists={playlists} />}
       </SectionWrapper>
     </main>
   );
